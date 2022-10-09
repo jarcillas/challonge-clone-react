3635ea3e5076a35c034a9cd941d9b9cac3597282
Jesreal Arcillas <jesrealarcillas@gmail.com>
2022-10-09 23:00:58 +0800

    add clearRest on Player click

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -9,7 +9,26 @@ const Player = ({
   const onPlayerClick = () => {
     let newRoundsData = [...roundsData];
     newRoundsData[roundNum][matchNum - 1] = name;
+    newRoundsData = clearRest(
+      newRoundsData,
+      roundNum + 2,
+      Math.ceil(matchNum / 2)
+    );
     setRoundsData(newRoundsData);
+  };
+
+  const clearRest = (prevRoundsData, clearRoundNum, clearPlayerNum) => {
+    if (clearRoundNum > roundsData.length) {
+      return prevRoundsData;
+    } else {
+      let nextRoundsData = [...prevRoundsData];
+      nextRoundsData[clearRoundNum - 1][clearPlayerNum - 1] = null;
+      return clearRest(
+        nextRoundsData,
+        clearRoundNum + 1,
+        Math.ceil(clearPlayerNum / 2)
+      );
+    }
   };
 
   return (
